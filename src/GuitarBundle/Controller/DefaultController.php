9b6f9fcabbcf1bd54e0870137516261fc1820851
Ercan K <ercankoese@gmx.de>
2017-12-01 01:28:04 +0100

    Methods have been created within the Guitar-/PlayberBundle. Implementation missing yet.

--- src/GuitarBundle/Controller/DefaultController.php
+++ src/GuitarBundle/Controller/DefaultController.php
@@ -8,9 +8,53 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/guitar")
      */
-    public function indexAction()
+    public function listAction()
+    {
+        return $this->render('GuitarBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/guitar/details/{iGuitarId}")
+     */
+    public function detailsAction($iGuitarId)
+    {
+        return $this->render('GuitarBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/guitar/add")
+     */
+    public function newFormAction()
+    {
+        return $this->render('GuitarBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/guitar/add")
+     * @method("post")
+     */
+    public function insertAction()
+    {
+        return $this->render('GuitarBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/guitar/edit/{iGuitarId}")
+     */
+    public function editFormAction($iGuitarId)
+    {
+        return $this->render('GuitarBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/guitar/edit")
+     * @method("post")
+     */
+    public function updateAction()
     {
         return $this->render('GuitarBundle:Default:index.html.twig');
     }
--- src/PlayerBundle/Controller/DefaultController.php
+++ src/PlayerBundle/Controller/DefaultController.php
@@ -8,9 +8,53 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/player")
      */
-    public function indexAction()
+    public function listAction()
+    {
+        return $this->render('PlayerBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/player/details/{iPlayerId}")
+     */
+    public function detailsAction($iPlayerId)
+    {
+        return $this->render('PlayerBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/player/add")
+     */
+    public function addFormAction()
+    {
+        return $this->render('PlayerBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/player/insert")
+     * @method("post")
+     */
+    public function insertAction()
+    {
+        return $this->render('PlayerBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/player/edit/{iPlayerId}")
+     */
+    public function editFormAction($iPlayerId)
+    {
+        return $this->render('PlayerBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/player/update")
+     * @method("post")
+     */
+    public function updateAction()
     {
         return $this->render('PlayerBundle:Default:index.html.twig');
     }
